2ef4280e2eb44d111bd7f0741846687df9cde97c
Wei <aszx70293@gmail.com>
2023-06-12 19:37:50 +0800

    20230612 - 1

---
+++ collection/src/com/practice/collection/list/ArrayListSample.java
@@ -0,0 +1,13 @@
+package com.practice.collection.list;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class ArrayListSample {
+    public static void main(String[] args) {
+        ArrayList<String> bookList = new ArrayList<String>();
+        bookList.add("三國演義");
+        bookList.add("水滸傳");
+        System.out.println(bookList);
+    }
+}
